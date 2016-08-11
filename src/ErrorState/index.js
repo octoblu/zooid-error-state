243842a0295cbfc9a1c7a2bdb40256e7126333dd
Jade Meskill <jade.meskill@gmail.com>
2016-08-11 15:31:50 -0700

    v1.3.0 add buttonKind

--- src/ErrorState/index.js
+++ src/ErrorState/index.js
@@ -4,35 +4,37 @@ import Button from 'zooid-button'
 
 import styles from './styles.css'
 
-const PrimaryAction = ({text, href, onClick}) => {
+const PrimaryAction = ({text, href, onClick, kind}) => {
   if (!text) return null
   if (!href && !onClick) return null
+  kind = kind || 'primary'
 
-  return <Button kind="primary" href={href} onClick={onClick}>{text}</Button>
+  return <Button kind={kind} href={href} onClick={onClick}>{text}</Button>
 }
 
-const ErrorState = ({ buttonText, className, description, href, onClick, title }) => {
+const ErrorState = ({ buttonKind, buttonText, className, description, href, onClick, title }) => {
   return (
     <div className={classNames(styles.wrapper, className)}>
       <h1 className={styles.title}>{title}</h1>
       <p className={styles.description}>{description}</p>
-      <PrimaryAction text={buttonText} onClick={onClick} href={href} />
+      <PrimaryAction kind={buttonKind} text={buttonText} onClick={onClick} href={href} />
     </div>
   )
 }
 
 ErrorState.defaultProps = {
   description: '',
-  title: 'OOPS'
+  title: 'OOPS',
 }
 
 ErrorState.propTypes = {
+  buttonKind: PropTypes.string,
   buttonText: PropTypes.string,
   className: PropTypes.string,
   description: PropTypes.string,
   href: PropTypes.string,
   onClick: PropTypes.func,
-  title: PropTypes.string
+  title: PropTypes.string,
 }
 
 export default ErrorState
